c1ef602642431ad06f9bd750cce5fb1d2cc29412
Villuntos-Skafikis <prawn.staging-0x@icloud.com>
2024-08-10 15:31:44 -0300

    codigos desenvolvidos baseados nos mesmos da aula 12

---
+++ src/aula12/LeituraDadosTeclado.java
@@ -0,0 +1,26 @@
+package aula12;
+
+import java.util.Scanner;
+
+public class LeituraDadosTeclado {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Olá, digite seu nome: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Digite sua idade: ");
+        int idade = sc.nextInt();
+
+        System.out.println("Digite sua altura: ");
+        double altura = sc.nextDouble();
+
+        System.out.println("Você tem animal de estimação? \n(true/false)");
+        boolean temPet = sc.nextBoolean();
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Tem animal de estimação? " + temPet);
+    }
+}

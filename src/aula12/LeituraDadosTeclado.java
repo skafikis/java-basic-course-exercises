f6232ffcf968b39b135e45718c37d10cdae43478
Villuntos-Windows11 <victorctmgeo@gmail.com>
2024-09-03 09:52:20 -0300

    :)

--- src/aula12/LeituraDadosTeclado.java
+++ src/aula12/LeituraDadosTeclado.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class LeituraDadosTeclado {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Olá, digite seu nome: ");
         String nome = sc.nextLine();
 
